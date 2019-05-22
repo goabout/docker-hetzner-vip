2c020ba67e4dc9c02651de17d9211af5f84a298b
Joost Cassee <joost@cassee.net>
2019-05-22 11:33:27 +0200

    Temporarily enable notify script debugging

--- notify.sh
+++ notify.sh
@@ -3,13 +3,13 @@
 set -euo pipefail
 
 if [ -z "$3" ]; then
-  echo "error: call with arguments TYPE NAME ENDSTATE" >&2
+  echo "error: call with arguments TYPE NAME STATE" >&2
   exit 1
 fi
 
 TYPE=$1
 NAME=$2
-ENDSTATE=$3
+STATE=$3
 
 if [ -z "$HCLOUD_TOKEN" ]; then
   echo "error: HCLOUD_TOKEN not set" >&2
@@ -26,7 +26,8 @@ if [ -z "$FLOATING_IP" ]; then
   exit 1
 fi
 
-if [ "$ENDSTATE" = "MASTER" ] ; then
+set -x
+if [ "$STATE" = "MASTER" ] ; then
   SERVER_ID=$(hcloud server list -o noheader -o columns=name,id | grep "^$NODE_NAME\>" | awk '{print $2}')
   FLOATING_IP_ID=$(hcloud floating-ip list -o noheader -o columns=ip,id | grep "^$FLOATING_IP\>" | awk '{print $2}')
   hcloud floating-ip assign "$FLOATING_IP_ID" "$SERVER_ID"
